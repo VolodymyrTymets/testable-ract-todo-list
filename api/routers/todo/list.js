df760d3a740bcf546141ffbc2163b7e3fcdb6035
volodymyrtymets <volodymyrtymets@gmail.com>
2021-02-21 19:42:34 +0200

    add searchTerm param

--- api/routers/todo/list.js
+++ api/routers/todo/list.js
@@ -11,6 +11,10 @@ const schema = {
       type: 'integer',
       description: 'The number of items to skip before starting to collect the result set',
     },
+    searchTerm: {
+      type: 'string',
+      description: 'The string to search todo item',
+    },
   },
   response: {
     201: {
@@ -29,15 +33,24 @@ const schema = {
   },
 };
 
+const symbols = /[&\/\\#,+()$~%.'":*?<>{}]/g;
+
 const list = (fastify, options) => async (request, reply) => {
   const { configs } = options;
-  let { limit, skip } = queryToObject(request.query);
+  let { limit, skip, searchTerm } = queryToObject(request.query);
   skip = skip ? parseInt(skip, 10) : 0;
   limit = parseInt(limit, 10);
   limit = limit && limit < configs.maxLimitPerQuery ? limit : configs.maxLimitPerQuery;
 
   const Todos = fastify.mongo.collection('todo');
-  const todos = await Todos.find({})
+  let query = { };
+  if (searchTerm) {
+    const replacedSearch = searchTerm.replace(symbols, "\\$&");
+    query = {
+      title: new RegExp(`${replacedSearch}`, 'i')
+    };
+  }
+  const todos = await Todos.find(query)
     .skip(skip)
     .limit(limit)
     .toArray();
