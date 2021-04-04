b73ffb2e151871c1e9a857f2058d4a7d6a84eff3
volodymyrtymets <volodymyrtymets@gmail.com>
2021-04-04 13:53:16 +0300

    forbid to create, edit, delete, todo in unauthentificated

--- api/routers/todo/index.js
+++ api/routers/todo/index.js
@@ -7,9 +7,18 @@ const remove = require('./remove');
 async function routes (fastify, options) {
   fastify.get('/todo', { schema: list.schema, }, list.router(fastify, options));
   fastify.get('/todo/:id', { schema: get.schema }, get.router(fastify, options));
-  fastify.post('/todo', { schema: create.schema }, create.router(fastify, options));
-  fastify.patch('/todo/:id', { schema: edit.schema }, edit.router(fastify, options));
-  fastify.delete('/todo/:id', { schema: remove.schema }, remove.router(fastify, options));
+  fastify.post('/todo', {
+    schema: create.schema,
+    preValidation: options.passport.authenticate('jwt', { session: false })
+  }, create.router(fastify, options));
+  fastify.patch('/todo/:id', {
+    schema: edit.schema,
+    preValidation: options.passport.authenticate('jwt', { session: false })
+  }, edit.router(fastify, options));
+  fastify.delete('/todo/:id', {
+    schema: remove.schema,
+    preValidation: options.passport.authenticate('jwt', { session: false })
+  }, remove.router(fastify, options));
 }
 
 module.exports = routes;
